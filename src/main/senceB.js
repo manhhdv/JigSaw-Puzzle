c3f6a71783c7ac85cb2100a8a04e18cefa4c354d
manhdvuetk63 <57102439+manhdvuetk63@users.noreply.github.com>
2020-06-26 13:37:24 +0700

    #5 Hiển thị hình ảnh lựa chọn chế độ và chuyển màn

---
+++ src/main/senceB.js
@@ -0,0 +1,25 @@
+class SenceB extends Phaser.Scene {
+
+    constructor() {
+        super("SenceB");
+    }
+    preload() {
+    }
+    create() {
+        var senceA=this.scene.get('SenceA');
+        senceA.setTitleGame(this);
+        var puzzlesBtn = this.add.image(game.config.width / 8, game.config.height / 1.71, 'lits', 'puzzlesBtn_c');
+        var acBtn = this.add.image(game.config.width / 1.8, game.config.height / 1.71+176, 'lits', 'acBtn_c');
+        acBtn.setRotation(-Math.PI/2);
+        var returnBtn=this.addReturnBtn(this);
+        returnBtn.setInteractive().on('pointerdown',function () {
+            this.scene.start('SenceA');
+        },this);
+    }
+    addReturnBtn(object) {
+        var returnBtn = object.add.image(game.config.width / 1.09, game.config.height / 1.2, 'lits', 'returnBtn_c');
+        var returnBtnr = object.add.image(game.config.width / 1.09, game.config.height / 1.2+94, 'lits', 'returnBtn_r');
+        returnBtnr.setRotation(-Math.PI/2);
+        return returnBtn;
+    }
+}
