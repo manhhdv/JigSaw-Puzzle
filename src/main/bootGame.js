af4477d77d100dc177908e7559a4c1f4f15a4098
manhdvuetk63 <57102439+manhdvuetk63@users.noreply.github.com>
2020-06-30 15:19:24 +0700

    #12 Phần khởi động dữ liệu trò chơi Bootgame

--- src/main/bootGame.js
+++ src/main/bootGame.js
@@ -3,12 +3,16 @@ class bootGame extends Phaser.Scene{
         super("BootGame");
     }
     preload(){
-        this.load.image('bg', 'assets/res/bg_0.png');
-        this.load.atlas('lits', 'assets/res/lit.png', 'assets/data_json/lit.json');
+        this.load.pack('pack_img','data_json/resImg/img.json','file_img');
+        this.load.pack('pack_puzzles','data_json/resImg/abcPuzzles.json','abcPuzzle');
+        this.load.pack('pack_puzzleora','data_json/resImg/abcPuzzleOra.json','abcPuzzleOra');
+        this.load.atlas('lits', 'assets/res/lit.png', 'data_json/lit.json');
+        this.load.atlas('abc', 'assets/res/lit.png', 'data_json/dataNameAbc.json');
     }
     create(){
         console.log('start');
+        // this.scene.start("PuzzleGame");
         this.scene.start("SenceA");
-
+        // this.scene.start("Puzzles");
     }
 }
