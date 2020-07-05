a7a5a9e9befef1cd5d8fba19e38127b9120b9e6b
manhdvuetk63 <natrihoatri1@gmail.com>
2020-07-05 15:06:46 +0700

    #10  làm chức năng tự chuyển màn

--- src/main/anphabetGame.js
+++ src/main/anphabetGame.js
@@ -1,7 +1,8 @@
 class AnphabetGame extends Phaser.Scene {
     constructor() {
         super("AnphabetGame");
-
+        this.initTime = 6;
+        this.nextRound = true;
     }
 
     init(data) {
@@ -18,21 +19,25 @@ class AnphabetGame extends Phaser.Scene {
         } else {
             this.id_img_pre = data.indexImg - 1;
         }
+        this.initTime;
+        this.nextRound;
     }
 
     create() {
+        console.log(this.initTime, this.nextRound);
         var abcSound = this.sound.add(this.id_img + 'Sound');
         abcSound.play({
             loop: false
         });
         this.setBg(this);
+        var sceneB = this.scene.get('SenceB');
+        sceneB.addCloud(this);
         var puzzles = this.scene.get('PuzzleGame');
-        this.setreturnBtn(this);
+        this.setreturnBtn(this, abcSound);
         puzzles.setAbcImange(this, this.id_img + 'PuzzleOra');
 
         var preBtn = this.setPreBtn(this).setInteractive();
         preBtn.on('pointerdown', function () {
-
             abcSound.stop();
             this.scene.sleep();
             this.scene.start("AnphabetGame", {frameAbc: this.listAbc, indexImg: this.id_img_pre});
@@ -52,6 +57,36 @@ class AnphabetGame extends Phaser.Scene {
             this.scene.sleep();
             this.scene.start("AnphabetGame", {frameAbc: this.listAbc, indexImg: this.id_img_next});
         }, this);
+        var pauseBtn = this.setPauseBtn(this).setInteractive();
+        pauseBtn.setInteractive({useHandCursor: true}).on('pointerdown', function () {
+            if (this.nextRound == true) {
+                this.initTime = 6;
+                this.sound.play("btn", {loop: false});
+                setTimeout(() => this.playbtn = this.add.image(950, game.config.height / 2, 'lits', 'pauseBtn_r'), 100);
+                this.nextRound = false;
+                // this.sound.stopAll();
+                // abcSound.play({loop: false});
+            } else {
+                this.initTime = 6;
+                this.sound.stopAll();
+                abcSound.play({loop: false});
+                this.playbtn.destroy();
+                this.playbtn = null;
+                this.nextRound = true;
+            }
+        }, this);
+        this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+                this.initTime -= 1;// One second
+                if (this.initTime <= 0&&this.nextRound==true ) {
+                    this.initTime = 6;
+                    this.scene.start("AnphabetGame", {frameAbc: this.listAbc, indexImg: this.id_img_next});
+                }
+            },
+            callbackScope: this,
+            loop: true
+        });
 
     }
 
@@ -61,10 +96,16 @@ class AnphabetGame extends Phaser.Scene {
         return bg;
     }
 
-    setreturnBtn(object) {
+    setPauseBtn(sceneName) {
+        var pauseBtn = sceneName.add.image(950, game.config.height / 2, 'lits', 'pauseBtn_c');
+        return pauseBtn;
+    }
+
+    setreturnBtn(object, abcSound) {
         let senceB = object.scene.get('SenceB');
         let re = senceB.addReturnBtn(object);
         re.setInteractive().on('pointerdown', function () {
+            abcSound.stop();
             this.clickSound(this);
             object.scene.sleep();
             object.scene.start('AnphabetCard');
@@ -73,7 +114,7 @@ class AnphabetGame extends Phaser.Scene {
     }
 
     setNextBtn(object) {
-        var nextBtn = object.add.image(950, 440, 'lits', 'nextBtn1');
+        var nextBtn = object.add.image(950, 500, 'lits', 'nextBtn1');
         nextBtn.setRotation(-Math.PI / 2);
         return nextBtn;
     }
--- src/main/bootGame.js
+++ src/main/bootGame.js
@@ -7,13 +7,45 @@ class bootGame extends Phaser.Scene {
         this.load.pack('pack_img', 'datagame/resImg/img.json', 'file_img');
         this.load.pack('pack_puzzles', 'datagame/resImg/abcPuzzles.json', 'abcPuzzle');
         this.load.pack('pack_puzzleora', 'datagame/resImg/abcPuzzleOra.json', 'abcPuzzleOra');
-        this.load.pack('pack_sound','datagame/soundData/sound.json','sound');
+        this.load.pack('pack_sound', 'datagame/soundData/sound.json', 'sound');
         this.load.atlas('lits', 'assets/res/lit.png', 'datagame/AtlasFrame/lit.json');
         this.load.atlas('abc', 'assets/res/lit.png', 'datagame/AtlasFrame/dataNameAbc.json');
 
     }
 
     create() {
+        console.log(this.parent);
         this.scene.start("WellcomeScene");
+    }
+
+    changeSceneEffect(sceneName, num) {
+        var blocks = sceneName.add.group({key: 'block', repeat: 300});
+
+        Phaser.Actions.GridAlign(blocks.getChildren(), {
+            width: 23,
+            cellWidth: 50,
+            cellHeight: 50,
+            x: 25,
+            y: 25
+        });
+        var _this = sceneName;
+        var i = 0;
+        blocks.children.iterate(function (child) {
+            _this.tweens.add({
+                targets: child,
+                scaleX: 0,
+                scaleY: 0,
+                alpha: 0,
+                y: '+=64',
+                angle: 180,
+                ease: 'Power3',
+                duration: 1000,
+                delay: (i * 100)
+            });
+            i++;
+            if (i % num === 0) {
+                i = 0;
+            }
+        });
     }
 }
